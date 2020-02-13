abbeed9f611982f333220743784d6818e9746bf3
Neffats <36647388+Neffats@users.noreply.github.com>
2020-02-13 16:19:45 +0000

    init commit

---
+++ ip.go
@@ -0,0 +1,101 @@
+package ip
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	ipv4Format = regexp.MustCompile("^((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\\.){3}(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])$")
+
+	// ErrInvalidFormat is returned for invalid IPv4 addresses.
+	ErrInvalidFormat = errors.New("Invalid IPv4 address format")
+)
+
+// IP is an integer representation of an IPv4 address.
+type IP uint32
+
+// NewIP converts a IPv4 address string to an IP object.
+func NewIP(address string) (*IP, error) {
+	// Check that we get a valid IPv4 address string.
+	if !ipv4Format.MatchString(address) {
+		return nil, ErrInvalidFormat
+	}
+
+	addrInt, err := AddrAtoi(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed to convert IPv4 string to int: %v", err)
+	}
+
+	out := IP(addrInt)
+
+	return &out, nil
+}
+
+// AddrAtoi returns the supplied IPv4 string as an unsigned 32 bit integer.
+func AddrAtoi(addr string) (uint32, error) {
+	// Separate the IP address into 4 octets.
+	splitAddress := strings.Split(addr, ".")
+
+	intAddress := make([]uint8, len(splitAddress))
+
+	var err error
+	var octet int
+	// Convert each octet from string to int.
+	for i, v := range splitAddress {
+		octet, err = strconv.Atoi(v)
+		if err != nil {
+			return 0, fmt.Errorf("failed to convert address to int: %v", err)
+		}
+		intAddress[i] = uint8(octet)
+		// TODO: Since we check the format at the start, do we need to check again?
+		if intAddress[i] > 255 {
+			return 0, fmt.Errorf("address cannot include a number greater than 255")
+		} else if intAddress[i] < 0 {
+			return 0, fmt.Errorf("address cannot include a number less than 0")
+		}
+	}
+
+	var address uint32 = 0
+	// Combine all the octets into a single integer.
+	for i := 0; i < len(intAddress)-1; i++ {
+		address = address | uint32(intAddress[i])
+		address = address << 8
+	}
+	address = address | uint32(intAddress[len(intAddress)-1])
+
+	return address, nil
+}
+
+// AddrItoa returns the supplied unsigned 32 bit integer as an IPv4 string.
+func AddrItoa(addr uint32) string {
+	var intAddr [4]uint8
+
+	intAddr[0] = uint8(addr >> 24)
+	intAddr[1] = uint8(addr >> 16)
+	intAddr[2] = uint8(addr >> 8)
+	intAddr[3] = uint8(addr)
+
+	b := new(bytes.Buffer)
+
+	for i := 0; i < len(intAddr)-1; i++ {
+		b.WriteString(strconv.Itoa(int(intAddr[i])))
+		b.WriteString(".")
+	}
+
+	b.WriteString(strconv.Itoa(int(intAddr[len(intAddr)-1])))
+
+	result := b.String()
+
+	return result
+}
+
+// Mask applies the supplied bit mask to the supplied address.
+func Mask(addr, mask *IP) *IP {
+	masked := IP(*addr & *mask)
+	return &masked
+}
---
+++ ip_test.go
@@ -0,0 +1,57 @@
+package ip
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewIP(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  IP
+		err   bool
+	}{
+		{name: "Valid ip address", input: "192.168.1.1", want: IP(3232235777), err: false},
+		{name: "Default route", input: "0.0.0.0", want: IP(0), err: false},
+		{name: "Invalid ip address format", input: "192.168.1.1.1", want: IP(0), err: true},
+		{name: "Invalid ip address", input: "192.168.1.256", want: IP(0), err: true},
+		{name: "Minus IP address", input: "-10.1.1.1", want: IP(0), err: true},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := NewIP(tc.input)
+			if err != nil {
+				if tc.err {
+					return
+				}
+				t.Errorf("got error when not expected: %v", err)
+			}
+			if tc.err {
+				t.Error("expected an error, but didn't get one")
+			}
+			if !reflect.DeepEqual(got, &tc.want) {
+				t.Errorf("got: %d, want: %d", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestMask(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []IP
+		want  IP
+	}{
+		{name: "192.168.1.3/255.255.255.0", input: []IP{IP(3232235779), IP(4294967040)}, want: IP(3232235776)},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := Mask(&tc.input[0], &tc.input[1])
+			if !reflect.DeepEqual(got, &tc.want) {
+				t.Errorf("got: %d, want: %d", *got, tc.want)
+			}
+		})
+	}
+}
